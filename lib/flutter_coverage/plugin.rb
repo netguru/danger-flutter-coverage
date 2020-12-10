bca041a55c920b3d9f72ab84156d88215354cd43
Kacper Kogut <kacper.kogut@netguru.com>
2020-12-10 23:01:47 +0100

    Add warnings and errors coverage

--- lib/flutter_coverage/plugin.rb
+++ lib/flutter_coverage/plugin.rb
@@ -1,7 +1,30 @@
 module Danger
   class DangerFlutterCoverage < Plugin
-      def warnin
-          warn 'It\'s alive'
+      
+      attr_accessor :lint_report_path
+      attr_accessor :coverage_report_path
+      
+      def parse_lint
+          input = File.open(lint_report_path)
+          filtered_input = input.each_line.map(&:strip).reject(&:empty?)
+
+          if filtered_input.detect { |element| element.include? "No issues found!" }
+              0
+          else
+              filtered_input
+          end
+      end
+
+      def warnings
+          filtered_input
+              .select { |line| line.start_with? "info" }
+              .length
+      end
+
+      def errors
+          filtered_input
+              .select { |line| line.start_with? "error" }
+              .length
       end
   end
 end
